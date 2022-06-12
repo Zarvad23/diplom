29f0a3e811f81e9cbb41b16b94dbd6f977a0b94c
Zarvad23 <zarifullinvadim@mail.ru>
2022-06-13 00:59:56 +0500

    2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-alert
+let moreWorks = document.getElementById('moreWorks');
+let ourWorks  = document.querySelector('.our-works .container');
+let Works  = document.querySelectorAll('.our-works .container .work');
+moreWorks.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    moreWorks.style.display = 'none'
+    ourWorks.style.gridTemplateRows = 'repeat(3, 400px)';
+    for (let i = 6; i<=Works.length - 1; i++) {
+        let work = Works[i]
+        work.style.opacity = '1'
+    }
+})
---
+++ js/viewImage.js
@@ -0,0 +1,34 @@
+let imageContainer = document.querySelectorAll('.our-works .container .work');
+
+let container = document.createElement('div');
+let image = document.createElement('img');
+image.style = `height: 100%; 
+               width: auto;
+               position: relative;
+               top: 0;
+               left: 50%;
+               transform: translateX(-50%);
+               `;
+container.style = `
+                   position: fixed;
+                   left: 0;
+                   top: 0;
+                   width: 100%;
+                   height: 100%;
+                   background-color: rgba(0,0,0,0.4);
+                   display: none;
+                   z-index: 10000;`;
+container.append(image);
+container.addEventListener('click', function() {
+    container.style.display = 'none'
+})
+document.body.append(container);
+
+
+for (let i = 0; i <= imageContainer.length; i++){
+    let currentImage = imageContainer[i];
+    currentImage.addEventListener('click', function() {
+        image.src = window.getComputedStyle(currentImage).backgroundImage.slice(35,53);
+        container.style.display = 'block'
+    })
+}
