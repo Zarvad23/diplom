5d1011f993845b8080c46b446546c7c601eb0495
Zarvad23 <zarifullinvadim@mail.ru>
2022-06-28 21:40:57 +0500

    2

--- php/sendMessage.php
+++ php/sendMessage.php
@@ -1,4 +1,19 @@
 <?php
-if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    mail("zarifullin.vadim@mail.ru", "Тест", "Текст письма \n 1-ая строчка \n 2-ая строчка \n 3-ая строчка");
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+    $to = 'zarifullin.vadim@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+    $subject = 'Обратный звонок'; //Загаловок сообщения
+    $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+    $headers .= "From: Отправитель <zarvad-support@zarvad.ru>\r\n"; //Наименование и почта отправителя
+    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 }
+?>
