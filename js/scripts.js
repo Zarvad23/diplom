271cd0b7ab104ffc72209746c153143b682a77fd
Zarvad23 <zarifullinvadim@mail.ru>
2022-06-28 22:35:04 +0500

    2

--- js/scripts.js
+++ js/scripts.js
@@ -11,34 +11,3 @@ moreWorks.addEventListener('click', function (evt) {
         work.style.opacity = '1'
     }
 })
-
-let block_show = false;
-
-function scrollTracking(){
-    if (block_show) {
-        return false;
-    }
-
-    let wt = $(window).scrollTop();
-    let wh = $(window).height();
-    let et = $('.active').offset().top;
-    let eh = $('.active').outerHeight();
-    let dh = $(document).height();
-
-    if (wt + wh >= et || wh + wt == dh || eh + et < wh){
-        block_show = true;
-
-        // Код анимации
-        $('.active span:eq(0)').show('fast', function(){
-            $(this).next().show('fast', arguments.callee);
-        });
-    }
-}
-
-$(window).scroll(function(){
-    scrollTracking();
-});
-
-$(document).ready(function(){
-    scrollTracking();
-});
